e4e9514ff00576af7890d3afe4e5414206310ae6
2uwls1126 <[2uwls1126@g.skku.edu]>
2022-06-15 11:15:10 +0900

    jsx 다루기

---
+++ react-study/src/Hello.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from 'styled-components';
+// import "./Hello.css"
+
+function Hello() {
+    const StyledButton=styled.button`
+        color: red;
+        backgound-color: gray;
+        `;
+    // const PracticeStyle={
+    //     marginTop: '10px',
+    //     backgroundColor: 'blue',
+    ;
+    return <StyledButton>나만의 버튼</StyledButton>
+        // <>
+        //     {/* <div class='red'>Hello World!</div>
+        //     <div class='red'>Hello World!</div>
+        //     <div class='red'>Hello World!</div> */}
+
+        // </>
+}
+    
+
+export default Hello;
